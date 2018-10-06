181c5d8ea0ee9d5459acbe8b2e03e54066ac33bc
aungtintlwin <go2lwin@yahoo.com>
2018-10-06 13:41:47 -0700

    innterHTML update

--- code.js
+++ code.js
@@ -1,36 +1,43 @@
-function getCar(honda) {
-    axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/honda?format=json`)
+function getCar(make) {
+    axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/${make}?format=json`)
     .then(results => {
-        const car = Results.data;
+        console.log(results)
+        const car = results.data.Results;  
+        console.log(car);
+    
+        for(var i=0; i < car.length; i++){
+          const carModel = car[i].Model_Name;
+          console.log(i+1, carModel);
 
-          
-          const carModel = document.getElementById('carModel');
-          // looping through object keys
-          Object.keys(car.Model_Name).forEach(car => {
-            console.log(car.carModel);
-            carModel.appendChild("");
-          }
-          )
-        return car
-        //console.log(car);
+          const models = document.getElementById("models");
+          let li = document.createElement('li');   
+          li.innerHTML = createListItem(carModel);
+          models.appendChild(li);
 
+        } 
+       
     })
     .catch(err => {
       console.error("Coding in Progress", err.message);
     })
-    }
+}
     
-    /**
-     * This handles the onsubmit on the form.
-     * @param {Object} event 
-     */
-    function handleSubmit(event) {
-      event.preventDefault();
-      // grabs the carname from the field
-      const carName = document.getElementById('carName').value;
-      // grabs the carModel list
-      const carModel = document.getElementById('carModel');
-      carModel.innerHTML = "";
-      getCar(carName);
-    }
+function createListItem (model) {
+  return `
+    <li class="list-group-item">${model}</li>
+  `
+}
+/**
+ * This handles the onsubmit on the form.
+ * @param {Object} event 
+ */
+function handleSubmit(event) {
+  event.preventDefault();
+  const make = document.getElementById('make').value;
+  const models = document.getElementById("models");
+  models.innerHTML = "";
+  getCar(make);
+  }
  
+ 
+getCar()
